953d083a03cdd230cc75fa134855e8f7d6a52ed7
Joey34kaze <ragnar1724@gmail.com>
2017-12-02 22:53:05 +0100

    uhh
    neki dost je metapodatkov

---
+++ commander/Assets/scripts/gameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour {
+
+
+    public void LoadLevel()
+    {
+        Application.LoadLevel("level_1");
+    }
+}
---
+++ commander/Assets/scripts/intro_camera_bobbing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class intro_camera_bobbing : MonoBehaviour {
+
+    float original;
+    public float bob = 70;
+    public float speed = 1;
+    void Start()
+    {
+        original = -40;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = Quaternion.Euler(0,Mathf.Sin(Time.time/speed) * bob + original, 0);
+    }
+}
