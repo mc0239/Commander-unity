2af5827a9ce11f898a0102c9ed7691f657731907
Joey34kaze <ragnar1724@gmail.com>
2017-12-04 14:00:41 +0100

    done i think
    camera : is child object of player
    player_controller : premika se z playercontroller scripto ki je nalimana na playerja. tudi gravitacija je tam not.
    terrain : uporabla se en .obj objekt. to bo ksnej treba zamenat verjetn.

---
+++ commander/Assets/scripts/playercontroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+    public float walkingSpeed = 5f;
+    public float jumpForce = 3f;
+    public float gravity = 10f;
+    private Vector3 moveDir = Vector3.zero;
+    private CharacterController cc;
+	// Use this for initialization
+	void Start () {
+        cc = gameObject.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (cc.isGrounded) {
+            moveDir = new Vector3(-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= walkingSpeed;
+            if (Input.GetButtonDown("Jump")) {
+                moveDir.y = jumpForce;
+            }
+
+        }
+
+        moveDir.y -= gravity * Time.deltaTime;
+
+        cc.Move(moveDir * Time.deltaTime);
+	}
+}
