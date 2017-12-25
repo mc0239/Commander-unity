eb20eac6bbaef6586528d3eb04e6cb88c57e4f5d
mc0239 <mc0239@student.uni-lj.si>
2017-12-25 18:41:04 +0100

    Working on level and player controls

--- commander/Assets/scripts/playercontroller.cs
+++ commander/Assets/scripts/playercontroller.cs
@@ -4,75 +4,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playercontroller : MonoBehaviour {
-    public float walkingSpeed = 5f;
-    public float jumpForce = 3f;
-    protected Vector3 gravity = Vector3.zero;
-    private Vector3 moveDir = Vector3.zero;
-    private CharacterController cc;
-    private Vector3 lastRotation=Vector3.zero;
+    public float moveSpeed = 7f;
+	public float jumpForce = 18f;
+	public float gravity = 6f;
 
+	private Vector3 movement = Vector3.zero;
 
-	// Use this for initialization
+	private CharacterController cc;
+
+	// Initialize
 	void Start () {
-        cc = gameObject.GetComponent<CharacterController>();
+        cc = GetComponent<CharacterController> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-        moveDir=handleKeyInput();
-        if (!moveDir.Equals(Vector3.zero)) transform.rotation = Quaternion.LookRotation(moveDir);
 
-        if (cc.isGrounded)
-        {
-            gravity = new Vector3(0,-2,0);
+		float moveDirection = Input.GetAxis ("Horizontal");
+		movement.z = moveDirection * moveSpeed;
 
-           
-            moveDir = Vector3.Normalize(moveDir);
-            moveDir *= walkingSpeed;
-            if (Input.GetButtonDown("Jump"))
-            {
-                gravity.y = jumpForce;
-            }
-            
 
-        }
-        else {
-            moveDir = Vector3.Normalize(moveDir);
-            moveDir *= walkingSpeed/2;
-            gravity += Physics.gravity * Time.deltaTime;
-        }
+		movement.y -= gravity / 10;
+		if (cc.isGrounded) {
+			movement.y = 0;
+		}
 
-        moveDir += gravity;
-        Debug.Log(moveDir);
-        cc.Move(moveDir * Time.deltaTime);
-	}
+		if (Input.GetButton("Jump") && cc.isGrounded) {
+			Debug.Log ("skač pizdica");
+			movement.y = jumpForce;
+		}
 
-    private Vector3 handleKeyInput()
-    {
-        if (-Input.GetAxis("Vertical") < 0 && Input.GetAxis("Horizontal") > 0)
-            return new Vector3(-1, 0, 1);
-        if (-Input.GetAxis("Vertical") < 0 && Input.GetAxis("Horizontal") == 0)
-            return new Vector3(-1, 0, 0);
-        if (-Input.GetAxis("Vertical") < 0 && Input.GetAxis("Horizontal") < 0)
-            return new Vector3(-1, 0, -1);
-
-        if (-Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") > 0)
-            return new Vector3(0, 0, 1);
-        if (-Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
-            return new Vector3(0, 0, 0);
-        if (-Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") < 0)
-            return new Vector3(0, 0, -1);
-
-        if (-Input.GetAxis("Vertical") > 0 && Input.GetAxis("Horizontal") > 0)
-            return new Vector3(1, 0, 1);
-        if (-Input.GetAxis("Vertical") > 0 && Input.GetAxis("Horizontal") == 0)
-            return new Vector3(1, 0, 0);
-        if (-Input.GetAxis("Vertical") > 0 && Input.GetAxis("Horizontal") < 0)
-            return new Vector3(1, 0, -1);
-
-        else {
-            Debug.Log("napaka pri smeri");
-            return Vector3.zero;
-        }
+		cc.Move (movement * Time.deltaTime);
+		if (movement.z != 0) {
+			// TODO: fix rotation
+			cc.transform.rotation = Quaternion.Euler(new Vector3(0, 90 * moveDirection - 90, 0));
+		}
     }
+
 }
