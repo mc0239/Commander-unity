9f390ac7c2354e3f72803579bccf9cd25553a924
mc0239 <mc0239@student.uni-lj.si>
2018-01-10 21:29:54 +0100

    Added Switch&Gate, Added minimap layer cubes, added banana collider & player score

---
+++ commander/Assets/scripts/Banana.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Banana : MonoBehaviour {
+	
+	public float TurnSpeed = 0.9f;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate(Vector3.up, TurnSpeed, Space.World);
+	}
+}
