0f8991af820a78480d8a782fe7e701f2f52719eb
sn3m <snak3.m@gmail.com>
2018-01-12 08:51:48 +0100

    added pause menu

--- commander/Assets/scripts/GameManager.cs
+++ commander/Assets/scripts/GameManager.cs
@@ -1,14 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    bool Paused = false;
 
-    public void LoadLevel()
+    public GameObject pauseMenu;
+        
+    void Update()
     {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (!Paused)
+            {
+                Pause();
+                Paused = true;
+            }
+            else
+            {
+                Unpause();
+                Paused = false;
+            }
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+    
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
         SceneManager.LoadScene("level_1");
+    }
+    
+    public void ToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("main_menu");
     }
     
 }
